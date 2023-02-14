a16cbdaa048c398ad3b08fe7645fef6c1e111826
mahdi05tazwar <83511272+mahdi05tazwar@users.noreply.github.com>
2023-02-15 02:09:27 +0600

    Add files via upload

---
+++ GUI2.java
@@ -0,0 +1,48 @@
+package GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI2 implements ActionListener{
+	JFrame frame;
+	JLabel label;
+	JPanel panel;
+	JButton button;
+	
+	public GUI2(){
+		frame = new JFrame();
+		button = new JButton("Click me");
+		button.addActionListener(this);
+		label = new JLabel("Not clicked");
+		panel = new JPanel();
+		
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		panel.setLayout(new GridLayout(2, 0));
+		panel.add(button);
+		panel.add(label);
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setTitle("HELLO");
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[]args) {
+		new GUI2();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == button) label.setText("CLICKED!");
+	}
+	
+}
